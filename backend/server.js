51bc54dc29051f56b906ad8938cf4b7494ae32bc
Bernst4315 <bernst4315@gmail.com>
2025-01-22 20:19:31 -0700

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
+require("dotenv").config()
+
 const express = require("express");
 const app = express(); //calls on express in ln 1
 
+
+//Global Middleware
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
 // routes
 app.get("/", (req,res) => {
-    res.json({mssg: "welcome to app"})
+    res.json({mssg: "welcome to app"});
 })
 
 //listen for requests
-const port = 3000;
-app.listen(port, () => {
+//const port = 3000;
+app.listen(process.env.PORT, () => {
     console.log("listening on port 3000")
 })
 
