bbbd588db4bf23948c8a8d700ca9b76e84b1e0fc
Bernst4315 <bernst4315@gmail.com>
2025-01-22 19:56:27 -0700

    1st route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express(); //calls on express in ln 1
 
+// routes
+app.get("/", (req,res) => {
+    res.json({mssg: "welcome to app"})
+})
+
 //listen for requests
 const port = 3000;
 app.listen(port, () => {
     console.log("listening on port 3000")
 })
+
